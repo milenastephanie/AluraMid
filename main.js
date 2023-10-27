077dcfd30820a4dae6b9f99eadfb28d1acb359e5
milenastephanie <106745448+milenastephanie@users.noreply.github.com>
2023-10-27 08:57:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play('');
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas{0} .sonclick = tocaSomPom;
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom ();
